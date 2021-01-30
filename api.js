e4791560f2c46edfece0b1169a40f3735b6993be
Gabriela Izidorio <gabrielaizidorio18@gmail.com>
2021-01-30 21:36:45 +0000

    Update JavaScript

--- api.js
+++ api.js
@@ -10,3 +10,91 @@ const Modal = {
         .remove('active')
     }
 }
+
+const transactions = [
+    {
+        id: 1,
+        description: 'Luz',
+        amount: -50000,
+        date: '23/01/2021'
+    },
+    {
+        id: 2,
+        description: 'Website',
+        amount: 500000,
+        date: '23/01/2021'
+    },
+    {
+        id: 3,
+        description: 'Internet',
+        amount: -20000,
+        date: '23/01/2021'
+    },
+]
+
+const Transaction = {
+    incomes() {
+        // somar as entradas
+    },
+    expenses() {
+        // somar as saídas
+    },
+    total() {
+        // entradas - saídas
+    }
+}
+
+const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+        DOM.transactionsContainer.appendChild(tr)
+    },
+
+    innerHTMLTransaction(transaction) {
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+        const amount = Utils.formatCurrency(transaction.amount)
+
+        const html = `
+        <tr>
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclass}">${amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td>
+                <img src="./assets/minus.svg" alt="Remover Transações">
+            </td>
+        </tr>
+        `
+
+        return html
+    },
+
+    updateBalance() {
+        
+    }
+}
+
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : ""
+
+        value = String(value).replace(/\D/g,"")
+
+        value = Number(value) / 100
+
+        value = value.toLocaleString("en", {
+            style: "currency",
+            currency: "GBP"
+        })
+
+        return signal + value
+    }
+}
+
+transactions.forEach(function(transaction) {
+    DOM.addTransaction(transaction)
+})
